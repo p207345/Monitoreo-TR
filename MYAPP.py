e6236bb4d8b0077d997de3dd8e661fed91392258
IgnacioRodriguez98 <99094740+IgnacioRodriguez98@users.noreply.github.com>
2022-04-12 22:04:01 -0600

    asdasd

--- MYAPP.py
+++ MYAPP.py
@@ -1,2 +1,14 @@
 import streamlit as st
 import pandas as pd
+
+with st.sidebar:
+    st.write(""" # Calculadora
+    ## Suma""")
+    num1 = st.slider("Escoge el primer número",0.0,300.0,15.0)
+    num2 = st.slider("Escoge el segundo número",0.0,300.0,15.0)
+    st.write("La suma de esos números es:", num1+num2)
+    st.write("## Multiplicación")
+    num3 = st.number_input("Escribe el primer número")
+    num4 = st.number_input("Escribe el segundo número")
+
+    st.write("La multiplicación de esos números es:", num3*num4)
