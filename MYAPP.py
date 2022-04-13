c712c8a04c2d262ade868d9ed6717e3ed1a647e2
IgnacioRodriguez98 <99094740+IgnacioRodriguez98@users.noreply.github.com>
2022-04-12 22:33:17 -0600

    q

--- MYAPP.py
+++ MYAPP.py
@@ -12,9 +12,13 @@ if database == '1':
         st.write("""# Has seleccionado la base de datos 1""")
         df = pd.read_csv('https://raw.githubusercontent.com/IgnacioRodriguez98/Monitoreo-TR/main/Data/norm.csv', header=None)
         st.write(df)
+
 elif database == '2':
     st.write("""# Has seleccionado la base de datos 2""")
-    
+    df = pd.read_csv('https://raw.githubusercontent.com/IgnacioRodriguez98/Monitoreo-TR/main/Data/normCH.csv', header=None)
+    st.write(df)
 
 else:
-        st.write("""# Has seleccionado la base de datos 3""")
+    st.write("""# Has seleccionado la base de datos 3""")
+    df = pd.read_csv('https://raw.githubusercontent.com/IgnacioRodriguez98/Monitoreo-TR/main/Data/normJA.csv', header=None)
+    st.write(df)
