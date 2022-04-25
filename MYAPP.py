004dd0e0398d553014c0203679c4d994aeb07985
p207345 <82604139+p207345@users.noreply.github.com>
2022-04-24 23:24:23 -0600

    m

--- MYAPP.py
+++ MYAPP.py
@@ -84,8 +84,8 @@ for i, row in p.iterrows():
     st.write(row)
     
     db.append(row)
-    time.sleep(1)
-    db = pd.DataFrame(db)
-    st.write(db)
+    time.sleep(.1)
+db = pd.DataFrame(db)
+st.write(db)
 st.write(p)
 
