aad2719ca75562e10b2b141660d86404bf833c52
p207345 <82604139+p207345@users.noreply.github.com>
2022-04-25 12:44:48 -0600

    m

--- MYAPP.py
+++ MYAPP.py
@@ -24,7 +24,7 @@ with st.sidebar:
 if database == '1':
         st.write("""# Has seleccionado la base de datos 1""")
         df = pd.read_csv('https://raw.githubusercontent.com/IgnacioRodriguez98/Monitoreo-TR/main/Data/norm.csv', header=None)
-        st.write(df.describe)
+        st.write(df)
 
 elif database == '2':
     st.write("""# Has seleccionado la base de datos 2""")
