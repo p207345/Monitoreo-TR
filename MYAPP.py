580edbf74919444525b6d98daad49e433cd4b1ac
p207345 <82604139+p207345@users.noreply.github.com>
2022-04-25 15:40:20 -0600

    m

--- MYAPP.py
+++ MYAPP.py
@@ -149,7 +149,7 @@ indices = pd.DataFrame(np.where(y_test_scores > 1.9))
 X_test = db[len(db)-round((len(db)/3)*2):]
 X_test.reset_index(inplace=True)
 
-fig2 = plt.fig(1)
+fig2 = plt.figure(1)
 plt.plot(X_test)
 plt.vlines([indices],0,710,"r")
 #plt.xlim(400,600)
