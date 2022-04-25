35892f6eb84294856fcca5faf01155a5dc37edd3
p207345 <82604139+p207345@users.noreply.github.com>
2022-04-24 23:37:16 -0600

    m

--- MYAPP.py
+++ MYAPP.py
@@ -81,7 +81,7 @@ if C2H2== False |H2 == False | C2H4 == False |CO == False |C2H6 ==False |CH4 == 
    p="""### No hay gases seleccionados, por favor selecciona al menos uno"""
 db = []
 for i, row in p.iterrows():
-    st.write(row)
+    #st.write(row)
     db.append(row)
     #time.sleep(.1)
 db = pd.DataFrame(db)
