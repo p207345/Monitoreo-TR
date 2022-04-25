c7bcd4733ef17e5d4722656f1bff842662d9c57d
p207345 <82604139+p207345@users.noreply.github.com>
2022-04-24 22:51:50 -0600

    m

--- MYAPP.py
+++ MYAPP.py
@@ -38,9 +38,9 @@ else:
     st.write(df)
 ##################### SELECCION DE GASES ######################
 
-#with st.sidebar:
-#   st.write("# Selecciona el tamaño de la ventana:")
-#    vent= st.slider("",1,len(df))
+with st.sidebar:
+   st.write("# Selecciona el tamaño de la ventana:")
+    vent= st.slider("",1,len(df))
 
 lista = []
 p= df[0]
