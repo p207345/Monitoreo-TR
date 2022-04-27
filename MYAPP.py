a4c372c4e3cc2d63b230852dc140656abae74c5e
p207345 <82604139+p207345@users.noreply.github.com>
2022-04-27 13:12:56 -0600

    m

--- MYAPP.py
+++ MYAPP.py
@@ -170,7 +170,7 @@ if con > 1:
     df_test['cluster'].value_counts()
 
     t = df_test.groupby('cluster').mean()
-    indices = pd.DataFrame(np.where(y_test_scores > t["score"].max()))
+    indices = pd.DataFrame(np.where(y_test_scores > (t["score"].max()-1)))
     X_test = db[round((len(db)/3)*2):]
     X_test.reset_index(inplace=True)
     st.write(X_test)
