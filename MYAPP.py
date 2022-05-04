f26f7270c4c036427706753f881ef271d23690cc
p207345 <82604139+p207345@users.noreply.github.com>
2022-05-03 19:02:36 -0600

    m

--- MYAPP.py
+++ MYAPP.py
@@ -22,19 +22,19 @@ with st.sidebar:
     C2H6 = st.checkbox("Etano")
     CH4 = st.checkbox("Metano")
 
-if database == '1':
+if database == '1 (2432 datos)':
         st.write("""# Has seleccionado la base de datos 1""")
-        df = pd.read_csv('https://raw.githubusercontent.com/IgnacioRodriguez98/Monitoreo-TR/main/Data/planta1.csv', header=None)
+        df = pd.read_csv('https://raw.githubusercontent.com/IgnacioRodriguez98/Monitoreo-TR/main/Data/planta2.csv', header=None)
         st.write(df)
 
-elif database == '2':
+elif database == '2 (9277 datos)':
     st.write("""# Has seleccionado la base de datos 2""")
-    df = pd.read_csv('https://raw.githubusercontent.com/IgnacioRodriguez98/Monitoreo-TR/main/Data/planta2.csv', header=None)
+    df = pd.read_csv('https://raw.githubusercontent.com/IgnacioRodriguez98/Monitoreo-TR/main/Data/planta3.csv', header=None)
     st.write(df)
 
 else:
     st.write("""# Has seleccionado la base de datos 3""")
-    df = pd.read_csv('https://raw.githubusercontent.com/IgnacioRodriguez98/Monitoreo-TR/main/Data/planta3.csv', header=None)
+    df = pd.read_csv('https://raw.githubusercontent.com/IgnacioRodriguez98/Monitoreo-TR/main/Data/planta1.csv', header=None)
     st.write(df)
 
 
