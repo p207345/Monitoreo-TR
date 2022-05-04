ac75fdd3cf482216fbb2519176170ffb2e0faa05
p207345 <82604139+p207345@users.noreply.github.com>
2022-05-03 19:37:30 -0600

    m

--- MYAPP.py
+++ MYAPP.py
@@ -158,10 +158,10 @@ for i, row in p.iterrows():
     #st.write(row)
     db.append(row)
     #time.sleep(1)
-st.write(db)
-db.drop([],inplace=True, axis=1)
-db = pd.DataFrame(db)
 
+db = pd.DataFrame(db)
+st.write(db)
+db.drop([0],inplace=True, axis=1)
 #st.write(db)
 #st.write(p)
 ############## Grafica
