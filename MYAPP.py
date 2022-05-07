fcf63ac807a2944c1267f116df9ab00a55d49d3d
p207345 <82604139+p207345@users.noreply.github.com>
2022-05-07 11:03:55 -0600

    m

--- MYAPP.py
+++ MYAPP.py
@@ -112,7 +112,7 @@ else:
     db = pd.DataFrame(db)
 
     ############## Grafica
-    fig = px.line(db,labels={1:C2H2,2:H2,3:C2H4,4:CO, 5:C2H6,6:CH4})
+    fig = px.line(db,labels={"1":C2H2,2:H2,3:C2H4,4:CO,5:C2H6,6:CH4})
     st.write(fig)
 
     #### AUTOENCODER
